89e6240652c8d68a9db9c505210da6b1bfe2e408
saifakh06 <114354515+saifakh06@users.noreply.github.com>
2024-07-22 13:46:07 +0530

    Update Flipkart.java

--- src/test/java/testCases/Flipkart.java
+++ src/test/java/testCases/Flipkart.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Flipkart {
 
 	public static void main(String[] args) {
-		WebDriver driver;
+		WebDriver driver ;
 		 System.setProperty("webdriver.chrome.driver", "D:\\JavaCodes\\SeleniumForMarch2024\\SeleniumForMarch2024\\Drivers\\chromedriver_125.exe");
 		 driver=new ChromeDriver();
 		 driver.manage().window().maximize();
